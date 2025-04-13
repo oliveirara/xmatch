a5096debdf7d6d04c0701110d604710a4ba3314e
Renan Alves de Oliveira <fisica.renan@gmail.com>
2025-04-13 19:03:22 +0000

    Formatted repository with Ruff

--- docs/example.py
+++ docs/example.py
@@ -1,13 +1,13 @@
-from xmatch import xmatch
 import pandas
 
-df = pandas.read_csv('data/table.csv', sep=';')
+from xmatch import xmatch
+
+df = pandas.read_csv("data/table.csv", sep=";")
 df.reset_index(inplace=True)
 
-df['snr'] = df['nufnu_5keV(erg.s-1.cm-2)']/df['nufnu_error_5keV(erg.s-1.cm-2)']
+df["snr"] = df["nufnu_5keV(erg.s-1.cm-2)"] / df["nufnu_error_5keV(erg.s-1.cm-2)"]
 
-cols = {'ra':'RA','dec':'DEC','id':'index'}
-#cnn = xmatch(df,df,columns_A=cols, columns_B=cols, method='nn')
-cgc = xmatch(df,df,columns_A=cols, columns_B=cols, radius=6/3600, method='gc', snr_column='snr')
-df = df.iloc[cgc.index] 
-
+cols = {"ra": "RA", "dec": "DEC", "id": "index"}
+# cnn = xmatch(df,df,columns_A=cols, columns_B=cols, method='nn')
+cgc = xmatch(df, df, columns_A=cols, columns_B=cols, radius=6 / 3600, method="gc", snr_column="snr")
+df = df.iloc[cgc.index]
